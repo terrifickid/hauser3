b17fb3f25fb5dc1870a8cc2f3308c4f320538b69
msplint <tk@terrifickid.net>
2022-05-22 16:02:17 -0500

    autocommit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -63,69 +63,68 @@
           allow="autoplay; fullscreen; picture-in-picture"
           allowfullscreen=""
         ></iframe>
+
+        <div
+          class="d-flex align-items-center"
+          style="position: relative; z-index: 200; padding: 25vh 0 25vh 0; color: black; "
+        >
+          <div class="container">
+            <div
+              style=" background: white; padding: 40px 50px 40px 50px"
+              class="col-12 col-lg-8 offset-lg-2"
+              v-ani="{ class: 'fade-in-bottom', delay: 0 }"
+            >
+              <h2
+                v-if="master.heading_1"
+                style="font-size: 16px;"
+                class="fnormal mb-3"
+                v-html="master.heading_1"
+              ></h2>
+              <h1
+                v-if="master.heading_2"
+                style="font-size: 56px; margin-bottom: 0.5rem;"
+                v-html="master.heading_2"
+              ></h1>
+              <h3
+                class="fnormal"
+                style="font-size: 16px; line-height: 23px;  margin-bottom: 2rem;"
+                v-if="master.heading_3"
+                v-html="master.heading_3"
+              ></h3>
+              <!--
+          <h2
+            class="d-inline-block mr-3"
+            v-for="(link, index) in master.hauser_links"
+            :key="index"
+            v-show="master.hauser_links.length"
+          >
+            <a v-if="link.type == 'Video'" @click="videoModalOpen()"
+              ><b-icon class="mr-1" icon="play-circle" /> {{ link.title }}</a
+            >
+            <a
+              v-if="link.type == 'Anchor'"
+              class="btn btn-md btn-outline-light"
+              @click="scrollTo(link.link)"
+              >{{ link.title }}</a
+            >
+            <a
+              v-if="link.type == 'Collection'"
+              class="btn btn-md btn-outline-light"
+              @click="scrollToCollection(link.link)"
+              >{{ link.title }}</a
+            >
+          </h2>
+        -->
+            </div>
+            <!-- end hauser-hero -->
+          </div>
+        </div>
       </div>
       <!-- end bg -->
       <div
         style="border: 0px solid red; position: relative; "
         ref="breakPoint"
       ></div>
-
-      <div class="container">
-        <div
-          style=" z-index: 200; position: relative; top: -7rem; margin-bottom: -10rem; background: white; padding: 40px 50px 40px 50px"
-          class="col-12 col-lg-8 offset-lg-2"
-          v-ani="{ class: 'fade-in-bottom', delay: 0 }"
-        >
-          <h2
-            v-if="master.heading_1"
-            style="font-size: 16px;"
-            class="fnormal mb-3"
-            v-html="master.heading_1"
-          >
-
-          </h2>
-          <h1
-            v-if="master.heading_2"
-            style="font-size: 56px; margin-bottom: 0.5rem;"
-            v-html="master.heading_2"
-          >
-
-          </h1>
-          <h3
-            class="fnormal"
-            style="font-size: 16px; line-height: 23px;  margin-bottom: 2rem;"
-            v-if="master.heading_3"
-            v-html="master.heading_3"
-          >
-
-          </h3>
-          <!--
-        <h2
-          class="d-inline-block mr-3"
-          v-for="(link, index) in master.hauser_links"
-          :key="index"
-          v-show="master.hauser_links.length"
-        >
-          <a v-if="link.type == 'Video'" @click="videoModalOpen()"
-            ><b-icon class="mr-1" icon="play-circle" /> {{ link.title }}</a
-          >
-          <a
-            v-if="link.type == 'Anchor'"
-            class="btn btn-md btn-outline-light"
-            @click="scrollTo(link.link)"
-            >{{ link.title }}</a
-          >
-          <a
-            v-if="link.type == 'Collection'"
-            class="btn btn-md btn-outline-light"
-            @click="scrollToCollection(link.link)"
-            >{{ link.title }}</a
-          >
-        </h2>
-      -->
-        </div>
-        <!-- end hauser-hero -->
-      </div>
 
       <AllArtworks ref="alla"></AllArtworks>
 
@@ -256,22 +255,22 @@ h1 {
   color: white;
   background: black;
   background-size: cover;
-  min-height: 75vh;
+
   position: relative;
   width: 100vw;
   border: 0;
   overflow: hidden;
 }
 #bg iframe {
-  width: 100vw;
-  height: 56.25vw; /* Given a 16:9 aspect ratio, 9/16*100 = 56.25 */
+  width: 120vw;
+  height: 100vh; /* Given a 16:9 aspect ratio, 9/16*100 = 56.25 */
   min-height: 100vh;
   min-width: 220vh; /* Given a 16:9 aspect ratio, 16/9*100 = 177.77 */
   position: absolute;
   z-index: 100;
-  top: 50%;
+
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, 0);
 }
 a,
 a:hover {
