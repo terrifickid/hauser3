817e9aa8448d90071eed52492f99ae609b447232
msplint <tk@terrifickid.net>
2022-05-28 00:13:06 -0500

    autocommit

--- src/views/Guide.vue
+++ src/views/Guide.vue
@@ -16,7 +16,7 @@
     <div class="container text-center">
       <div class="col-md-10 offset-md-1" style="padding: 6rem 0">
         <h1>Your Guide to Zürich Exhibitions</h1>
-        <p class="norm fminion">
+        <p class="norm fminion coff">
           'Just a short trip from Basel is the city of Zurich, the place where
           our earliest steps into the art world took place 30 years ago.
           Experience an exceptional range of exhibitions in our three Zurich
@@ -29,47 +29,67 @@
 
     <div class="container">
       <div style="padding: 0 0 4rem 0">
-        <img src="Map.png" class="img-fluid" />
+        <img src="Map.png" class="img-fluid d-none d-lg-block" />
+        <img
+          src="map-tablet.jpg"
+          class="img-fluid d-none d-md-block d-lg-none"
+        />
+        <img src="map-mobile.jpg" class="img-fluid d-md-none" />
       </div>
     </div>
 
     <div class="container map">
       <ul class="row">
-        <li class="col-lg-4 col-xl d-flex align-items-center size2 mb-4">
+        <li
+          class="col-sm-6 col-lg-4 col-xl d-flex align-items-center size2 mb-4"
+        >
           <span class="bgblue rounded num1 text-white size1"
             ><span>01</span></span
           >Bahnhofstrasse
         </li>
-        <li class="col-lg-4 col-xl d-flex align-items-center size2 mb-4">
+        <li
+          class="col-sm-6 col-lg-4 col-xl d-flex align-items-center size2 mb-4"
+        >
           <span class="bgblue rounded num1 text-white size1"
             ><span>03</span></span
           >Rämistrasse
         </li>
-        <li class="col-lg-4 col-xl d-flex align-items-center size2 mb-4">
+        <li
+          class="col-sm-6 col-lg-4 col-xl d-flex align-items-center size2 mb-4"
+        >
           <span class="bgblue rounded num1 text-white size1"
             ><span>02</span></span
           >Limmatstrasse
         </li>
-        <li class="col-lg-4 col-xl d-flex align-items-center size2 mb-4">
+        <li
+          class="col-sm-6 col-lg-4 col-xl d-flex align-items-center size2 mb-4"
+        >
           <span class="bgblue rounded num1 text-white size1"
             ><span>04</span></span
           >Zürich Main Station
         </li>
       </ul>
       <p class="text-center">
-        <a href="#">open in google maps</a>
+        <a href="#" class="d-block d-md-inline-block bbutton mt-4"
+          >open in google maps</a
+        >
       </p>
     </div>
 
-    <hr />
+    <hr style="margin: 6rem;" />
     <img class="img-fluid" src="ss1.png" />
     <div class="container">
       <div
         style="padding: 6rem 0; text-align: center"
         class="col-md-10 offset-md-1"
       >
+        <span
+          style="margin: auto; margin-bottom: 1.25rem;"
+          class="bgblue rounded num1 text-white size1"
+          ><span>01</span></span
+        >
         <h1>Around Bahnhofstrasse</h1>
-        <p class="norm">
+        <p class="norm fminion coff">
           Zurich’s Bahnhofstrasse is undoubtably the most famous landmark of the
           historic city center. Centrally located close to Lake Zurich and
           adjacent to the landmark Baur au Lac hotel, visitors of this
@@ -83,33 +103,43 @@
       </div>
     </div>
 
+    <div
+      style="background: url('divider.png') repeat-y; background-position: center center; height: 15vh;"
+    ></div>
     <div class="row no-gutters">
-      <div class="col">
+      <div class="col-12 col-sm pr-4 pr-sm-0">
         <img class="img-fluid" src="image124.png" />
-        <p class="caption text-center">
+        <p class="d-none d-sm-block caption text-center">
           {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
       </div>
-      <div class="col">
-        <img style="margin-top: 5vw;" class="img-fluid" src="image125.png" />
+      <div class="col-12 col-sm pl-4 pl-sm-0">
+        <img class="img-fluid mt-sm-4 mt-md-5" src="image125.png" />
+      </div>
+      <div class="col-12 d-sm-none caption mt-1 px-4">
+        {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       </div>
     </div>
 
     <div class="container" style="margin-top:6rem;">
-      <h1 class="text-center">
+      <h1 class="text-center fminion size0">
         Facing Infinity.<br />
-        Alberto Giacometti<br />
-        & Pablo Picasso
+        <i
+          >Alberto Giacometti<br />
+          & Pablo Picasso</i
+        >
       </h1>
-      <p class="text-center">Zürich, Bahnhofstrasse 1</p>
+      <p class="text-center size2">
+        Zürich, Bahnhofstrasse 1
+      </p>
     </div>
 
-    <div class="container">
+    <div class="container d-none d-lg-block">
       <div class="row">
         <div class="col d-flex justify-content-center">
           <div class="max">
-            <p>01.</p>
-            <p>
+            <p class="cblue size1 mb-1">01.</p>
+            <p class="size1 fminion mb-2">
               {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Commodo orci aliquam turpis tristique et risus. Fames dignissim
               elementum.
@@ -120,8 +150,8 @@
         <div class="col d-flex justify-content-center">
           <div class="max">
             <img src="1_2.png" class="img-fluid" style="margin-bottom: 1rem;" />
-            <p>02.</p>
-            <p>
+            <p class="cblue size1 mb-1">02.</p>
+            <p class="size1 fminion mb-2">
               {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Commodo orci aliquam turpis tristique et risus. Fames dignissim
               elementum.
@@ -132,6 +162,7 @@
     </div>
 
     <div
+      class="d-none d-lg-block"
       style="border-top: 1px solid #E9E9E9; border-bottom: 1px solid #E9E9E9;"
     >
       <div class="container">
@@ -143,8 +174,8 @@
           </div>
           <div class="col d-flex align-items-end">
             <div class="max" style="padding-left: 0;">
-              <p>03.</p>
-              <p>
+              <p class="cblue size1 mb-1">03.</p>
+              <p class="size1 fminion mb-2">
                 {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing
                 elit. Commodo orci aliquam turpis tristique et risus. Fames
                 dignissim elementum.
@@ -155,11 +186,12 @@
       </div>
     </div>
 
-    <div class="container-fluid">
+    <div class="container-fluid d-none d-lg-block">
       <div class="row no-gutters" style="border-bottom: 1px solid #E9E9E9">
         <div class="col" style="border-right: 1px solid #E9E9E9">
           <div class="max">
-            <p>
+            <p class="cblue size1 mb-1">04.</p>
+            <p class="size1 fminion mb-4">
               {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Commodo orci aliquam turpis tristique et risus. Fames dignissim
               elementum.
@@ -169,13 +201,96 @@
         </div>
         <div class="col-7 d-flex align-items-center">
           <div class="max">
-            <img src="1_5.png" style="width: 100%; padding-top: 1rem;" />
-            <p style="padding: 1rem 0 0rem 10vw;">
-              {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Commodo orci aliquam turpis tristique et risus. Fames dignissim
-              elementum.
-            </p>
+            <img src="1_5.png" style="width: 100%; padding-top: 3rem;" />
+
+            <div style="padding: 1rem 0 0rem 10vw;">
+              <p class="cblue size1 mb-1 mt-2">05.</p>
+              <p class="size1 fminion">
+                {Caption} Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit. Commodo orci aliquam turpis tristique et risus. Fames
+                dignissim elementum.
+              </p>
+            </div>
           </div>
+        </div>
+      </div>
+    </div>
+
+    <h1 class="text-center my-5 d-lg-none">[ swiperjs ]</h1>
+
+    <img src="ss2.jpg" class="img-fluid" style="margin-top: 4rem;" />
+    <div class="container">
+      <div
+        style="padding: 6rem 0; text-align: center"
+        class="col-md-10 offset-md-1"
+      >
+        <span
+          style="margin: auto; margin-bottom: 1.25rem;"
+          class="bgblue rounded num1 text-white size1"
+          ><span>02</span></span
+        >
+        <h1>Around Rämistrasse</h1>
+        <p class="norm fminion coff">
+          The headquarters of Hauser & Wirth Publishers opened in 2019 in the
+          historic building that once housed the Swiss publisher Emil and Emmie
+          Oprecht. Enjoy a range of art books from the gallery’s imprint
+          alongside the exhibition ‘About, by or of Marcel Duchamp,’ set up by
+          fluid editions. At the bottom of Rämistrasse is the legendary ODEON,
+          the perfect cafe no matter what time of day. And don’t miss a visit to
+          the internationally renowned Kunsthaus Zürich and its newly opened
+          extension, designed by David Chipperfield.
+        </p>
+      </div>
+    </div>
+
+    <img src="ss3.jpg" class="img-fluid" style="margin-top: 4rem;" />
+    <div class="container">
+      <div
+        style="padding: 6rem 0; text-align: center"
+        class="col-md-10 offset-md-1"
+      >
+        <span
+          style="margin: auto; margin-bottom: 1.25rem;"
+          class="bgblue rounded num1 text-white size1"
+          ><span>03</span></span
+        >
+        <h1>Around Limmatstrasse</h1>
+        <p class="norm fminion coff">
+          Formerly a brewery, the Löwenbräu building is a unique art center in
+          the Zürich West district. Where beer was once brewed, there are now
+          museums, gallery, publishers, cultural institutions and a restaurant.
+          In 1996 Hauser & Wirth Zürich, designed by Selldorf Architects, was
+          launched in this unique setting. Don’t miss our exhibitions of new
+          works by British artist Sir Frank Bowling and early works from the
+          1960s by Jack Whitten. While at Löwenbräu Kunst, make sure not to miss
+          our neighbors including Kunsthalle Zürich and Migros Museum or
+          Contemporary Art.
+        </p>
+      </div>
+    </div>
+
+    <div class="container">
+      <img src="up-icon.svg" class="mr-2" />
+      Scroll to Top
+    </div>
+
+    <div
+      style="background: #F8F8F8; padding: 5rem 0; margin-top: 5rem; margin-bottom: 5rem;"
+    >
+      <div class="container">
+        <div class="col-10 offset-1">
+          <h2 class="size3">Getting Around</h2>
+          <p class="size4 fminion">
+            {Short intro - 140 - 280 characters} Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Vivamus ullamcorper maximus mauris, in
+            accumsan velit pharetra scelerisque. Nunc tempus tincidunt nunc
+            vitae pulvinar. Fusce luctus sapien leo, vitae efficitur orci
+            finibus cursus. Sed bibendum tortor lobortis.
+          </p>
+          <ul style="font-size: 20px;">
+            <li class="my-5"><img src="tram.svg" class="mr-2" /> By Tram</li>
+            <li class="my-5"><img src="foot.svg" class="mr-2" /> By Foot</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -185,6 +300,7 @@
 <script>
 import Header from "@/components/Header.vue";
 import Footer from "@/components/Footer.vue";
+
 export default {
   components: {
     Header,
@@ -198,6 +314,11 @@ export default {
 };
 </script>
 <style>
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
 #bg {
   color: white;
   background: black;
@@ -219,14 +340,7 @@ export default {
   left: 50%;
   transform: translate(-50%, 0);
 }
-h1 {
-  font-size: 48px;
-  margin-bottom: 1rem;
-}
-p.norm {
-  font-size: 31px;
-  line-height: 48px;
-}
+
 .map ul {
   margin: 0;
   padding: 0;
@@ -255,25 +369,106 @@ p.norm {
   color: white;
 }
 
-.num1 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 3rem;
-  height: 3rem;
-  margin-right: 1rem;
-}
-
 .num1 span {
   position: relative;
   top: 1px;
 }
 
-.size1 {
+.bbutton {
+  border: 2px solid black;
+  border-radius: 0.25rem;
+  padding: 1rem 5rem;
+
+  text-transform: uppercase;
+  color: black;
+  font-size: 14px;
+}
+.bbutton:hover {
+  color: white;
+  background: black;
+  text-decoration: none;
+}
+
+.cblue {
+  color: #0995ae;
+}
+.coff {
+  color: #4e4e4e;
+}
+
+p {
+  margin: 0;
+}
+
+h1 {
+  font-size: 31px;
+  margin-bottom: 1rem;
+}
+p.norm {
   font-size: 18px;
+  line-height: 26px;
+}
+
+.size1 {
+  font-size: 12px;
 }
 
 .size2 {
-  font-size: 20px;
+  font-size: 16px;
+}
+
+.num1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 2rem;
+  height: 2rem;
+  margin-right: 1rem;
+}
+
+.size0 {
+  font-size: 39px;
+}
+@media (min-width: 576px) {
+}
+@media (min-width: 768px) {
+}
+@media (min-width: 992px) {
+  h1 {
+    font-size: 48px;
+    margin-bottom: 1rem;
+  }
+  p.norm {
+    font-size: 31px;
+    line-height: 48px;
+  }
+  .size0 {
+    font-size: 61px;
+  }
+  .size1 {
+    font-size: 18px;
+  }
+
+  .size2 {
+    font-size: 20px;
+  }
+
+  .size3 {
+    font-size: 48px;
+  }
+
+  .size4 {
+    font-size: 25px;
+  }
+  .num1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 3rem;
+    height: 3rem;
+    margin-right: 1rem;
+  }
+}
+@media (min-width: 1200px) {
 }
 </style>
